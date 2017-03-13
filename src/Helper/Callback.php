544318fc38aee84189c026b8cf82c6a8e20166d1
Beno!t POLASZEK <bpolaszek@gmail.com>
2017-03-13 14:51:46 +0100

    Moved classes in subdirectories

--- src/Helper/Callback.php
+++ src/Helper/Callback.php
@@ -2,8 +2,6 @@
 
 namespace BenTools\Specification\Helper;
 
-use BenTools\Specification\Helper\CallbackSpecification;
-
 if (!function_exists(sprintf('%s\\callback', __NAMESPACE__))) {
     function callback(callable $callback)
     {
